2f6d6a2d51fb1f10fce7bce9ee519542dbd491e2
Aigerim-serdaliyeva <33958074+Aigerim-serdaliyeva@users.noreply.github.com>
2018-06-08 19:53:01 +0600

    builded

--- app/js/main.js
+++ app/js/main.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
   var $header = $(".header");
   var $menu = $(".main-menu");
   var utms = parseGET();
-  var headerHeight = 91;
+  var headerHeight = 94;
   var $hamburger = $(".hamburger");
 
   if (utms && Object.keys(utms).length > 0) {
@@ -22,7 +22,7 @@ $(document).ready(function () {
   } 
 
   if ($wnd.width() < 992) {
-    headerHeight = 91;
+    headerHeight = 94;
   }
 
   if ($wnd.width() > 479) {
